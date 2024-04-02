78a9890f23a8c04297172927ab2c5fdd30e13da9
giosciafra <francescogioscia8@gmail.com>
2024-04-02 18:37:48 +0200

    layout personalizzato

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ class Movie
     public $year;
 
     public $genre;
+    public $poster;
     // costruttore    
     /**
      * __construct
@@ -18,12 +19,13 @@ class Movie
      * @param  mixed $_genre
      * 
      */
-    public function __construct($_title, $_language, $_year, $_genre)
+    public function __construct($_title, $_language, $_year, $_genre, $_poster)
     {
         $this->title = $_title;
         $this->language = $_language;
         $this->year = $_year;
         $this->genre = $_genre;
+        $this->poster = $_poster;
     }
 
 
@@ -58,13 +60,13 @@ class Genre
 }
 
 $movie1Genre = new Genre("Sci-fi", "Avventura");
-$movie1 = new Movie("Interstellar", "Inglese", 2014, $movie1Genre);
+$movie1 = new Movie("Interstellar", "Inglese", 2014, $movie1Genre, "img/interstellar.jpg");
 
 $movie2Genre = new Genre("Sci-fi", "Azione");
-$movie2 = new Movie("Inception", "Inglese", 2010, $movie2Genre);
+$movie2 = new Movie("Inception", "Inglese", 2010, $movie2Genre, "img/inception.jpg");
 
 $movie3Genre = new Genre("Commedia", "Cinema fantastico");
-$movie3 =  new Movie("Barbie", "Inglese", 2023, $movie3Genre);
+$movie3 =  new Movie("Barbie", "Inglese", 2023, $movie3Genre, "./img/barbie.jpg");
 
 
 // creo array di film
@@ -89,23 +91,31 @@ $movies = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
+<body class=" bg-dark ">
 
-    <h1>Movies</h1>
+    <h1 class="d-flex justify-content-center text-warning py-2 text-uppercase  ">Movies</h1>
 
-    <ul>
-        <?php
-        foreach ($movies as $movie) {
-            echo "
-        <li>
-             " . $movie->getTitle() . " <br>
-             " . $movie->language . " <br>
-             " . $movie->year . " <br>
-             " . $movie->genre->getGenre() . "
-        </li>";
-        }
-        ?>
-    </ul>
+    <div class="container">
+        <div class="row justify-content-center ">
+            <?php
+            foreach ($movies as $movie) {
+                echo "
+            <div class='col-md-3'>
+                <div class='card'>
+                    <img src='" . $movie->poster . "' class='card-img-top' alt='" . $movie->getTitle() . "'>
+                    <div class='card-body'>
+                        <h3 class='card-title'><strong>" . $movie->getTitle() . "</strong></h3>
+                        <p class='card-text'><strong>Lingua:</strong> " . $movie->language . "</p>
+                        <p class='card-text'><strong>Anno uscita:</strong> " . $movie->year . "</p>
+                        <p class='card-text'><strong>Genere:</strong> " . $movie->genre->getGenre() . "</p>
+                    </div>
+                </div>
+            </div>";
+            }
+            ?>
+        </div>
+    </div>
+
 
     <!-- script bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
