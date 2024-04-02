f5098c19ccd34e94fbd2863e70ba9019d2bc1b8e
giosciafra <francescogioscia8@gmail.com>
2024-04-02 16:51:50 +0200

    feat: definito un metodo per avere l'anno, istanziati due oggetti 'movie' e da li creato un array di 'movie' (dove vi ho aggiunto un terzo elemento 'movie') e stampato a schermo i risultati

--- index.php
+++ index.php
@@ -21,8 +21,32 @@ class Movie
         $this->language = $_language;
         $this->year = $_year;
     }
+
+    // Metodo per ottenere il titolo del film
+    public function getTitle()
+    {
+        return $this->title;
+    }
 }
+
+// istanziamento di due oggetti 'Movie'
+
+$movie1 = new Movie("Interstellar", "Inglese", 2014);
+// var_dump($movie1);
+$movie2 = new Movie("Inception", "Inglese", 2010);
+// var_dump($movie2);
+
+
+// creo array di film
+$movies = [
+    $movie1,
+    $movie2,
+    new Movie("Barbie", "Inglese", 2023)
+];
+
+// var_dump($movies);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -30,11 +54,30 @@ class Movie
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Movies</title>
+    <!-- bootsrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
 
+    <h1>Film</h1>
+
+    <ul>
+        <?php
+        foreach ($movies as $movie) {
+            echo "
+        <li>
+            " . $movie->getTitle() . ", <br>
+             " . $movie->language . ", <br>
+             " . $movie->year . "
+        </li>";
+        }
+        ?>
+    </ul>
+
+    <!-- script bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
